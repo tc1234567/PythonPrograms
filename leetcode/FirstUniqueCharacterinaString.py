77e708b48dbb31c35a487c439480c769605de9f1
Tanvi C <86094343+tc1234567@users.noreply.github.com>
2022-09-22 18:01:46 -0700

    Create FirstUniqueCharacterinaString.py

---
+++ leetcode/FirstUniqueCharacterinaString.py
@@ -0,0 +1,25 @@
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        
+        s = list(s)
+
+        d = {}
+
+        for letter in s:
+            if letter in d:
+                d[letter] += 1
+            else:
+                d[letter] = 1
+
+        d_keys = []
+
+        for i in d.keys():
+
+            if d[i]==1:
+                d_keys.append(i)
+
+        if not d_keys:
+            return -1
+
+        result = d_keys[0]
+        return s.index(result)
